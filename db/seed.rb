58339bd9740c93ecfcd379762a8ecac007d75a3f
Josh-Stillman <joshstillman@gmail.com>
2018-03-08 13:02:22 -0500

    Done.

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,26 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require 'require_all'
+require_all 'config'
+require 'csv'
+require 'sqlite3'
+require 'pry'
+
+database_connection = SQLite3::Database.new('db/daily.db')
+
+
+create_schema = <<-SQL
+  CREATE TABLE IF NOT EXISTS daily
+  (id INTEGER PRIMARY KEY,
+    year INTEGER,
+    occupation TEXT,
+    show_date TEXT,
+    category TEXT,
+    name TEXT
+  )
+SQL
+
+database_connection.execute(create_schema)
+
+CSV.foreach("/Users/MaxGentle/Development/labs/sql/bonus-sql-challenge-web-103017/daily_show_guests.csv", headers: true) do |row|
+    database_connection.execute("INSERT INTO daily (year, occupation, show_date, category, name) VALUES ( ?, ?, ?, ?, ? )", row.fields)
+end
